0f60ecd41afb0e8e3753a6af1c4aa6415e105683
bzveriok <bohdan.zverok@blackbird.io>
2023-12-25 14:41:39 +0200

    added new files

--- Apps.AmazonTranslate/Models/RequestModels/TranslateFileRequest.cs
+++ Apps.AmazonTranslate/Models/RequestModels/TranslateFileRequest.cs
@@ -1,13 +1,13 @@
 ﻿using Apps.AmazonTranslate.Models.RequestModels.Base;
 using Blackbird.Applications.Sdk.Common;
-using File = Blackbird.Applications.Sdk.Common.Files.File;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.AmazonTranslate.Models.RequestModels;
 
 public class TranslateFileRequest : TranslateRequest
 {
     [Display("File", Description = "File in HTML, TXT, or DOCX format.")]
-    public File File { get; set; }
+    public FileReference File { get; set; }
     
     [Display("Output filename", Description = "Name of the resulting file without an extension.")]
     public string? OutputFilename { get; set; }
